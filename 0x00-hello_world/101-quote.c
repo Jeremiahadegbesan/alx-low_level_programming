0b2cfced6d43ff44e9e3f740dba280500f881eb7
Jeremiah Adegbesan <jeremiahadegbesan@gmail.com>
2022-12-09 13:31:33 -0800

    BI want to check why I'm receiving error messages

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#include <unistd.h>
+
+/**
+ * main - You have to be a genius before you could understand UNIX
+ * Return: Always
+*/
+
+int main(void)
+
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - C programme that prints a line of code using puts*
+ * Return: 0
+ *
+*/
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+}
